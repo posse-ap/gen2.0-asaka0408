8ee21f068ae299f6ba7441a1c20dfd7b350df582
Asaka Ishikawa <sakanadaisuki0408@softbank.ne.jp>
2021-07-29 13:35:04 +0900

    正解エリアの作成とcssの順番を整えた

--- kuizy.js
+++ kuizy.js
@@ -2,11 +2,10 @@
 
 let correct = document.getElementById("correct");
 
+
 correct.onclick = function () {
     correct.classList.add("correct");
+    
 }
 
 
-
-let quiz = document.getElementById('quiz');
-quiz.appendChild('div');
