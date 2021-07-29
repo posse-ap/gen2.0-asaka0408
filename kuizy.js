40dfeb1ae6576bc8cfb5d062dd43d5bca708773a
Asaka Ishikawa <sakanadaisuki0408@softbank.ne.jp>
2021-07-29 11:24:13 +0900

    正解を押した時背景が青くなって文字が白くなるようにした

---
+++ kuizy.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let correct = document.getElementById("correct");
+
+correct.onclick = function () {
+    correct.classList.add("correct");
+}
+
+
+
+let quiz = document.getElementById('quiz');
+quiz.appendChild('div');
