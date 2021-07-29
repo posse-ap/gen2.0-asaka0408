9d6bf7665d1b3a74a7487a921811a1825d6f304d
Asaka Ishikawa <sakanadaisuki0408@softbank.ne.jp>
2021-07-29 14:12:39 +0900

    first commit

--- kuizy.js
+++ kuizy.js
@@ -2,10 +2,17 @@
 
 let correct = document.getElementById("correct");
 
-
 correct.onclick = function () {
     correct.classList.add("correct");
-    
 }
 
+let resultBox = document.getElementById('resultBox')
+
+ function toggleStyle () {
+    style.display = 'none';
+}
+
+function () {
+    style.display = 'box';
+}
 
