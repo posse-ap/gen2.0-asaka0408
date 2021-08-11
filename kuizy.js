d33ab264e4f6713c24bd7f09b9b6fc2e486760a6
Asaka Ishikawa <sakanadaisuki0408@softbank.ne.jp>
2021-08-11 19:52:59 +0900

    不正解を押した時の処理

--- kuizy.js
+++ kuizy.js
@@ -4,15 +4,49 @@ let correct = document.getElementById("correct");
 
 correct.onclick = function () {
     correct.classList.add("correct");
+    showBox();
+    fail1.style.pointerEvents = 'none';
+    fail2.style.pointerEvents = 'none';
+    correct.style.pointerEvents = 'none';
 }
 
-let resultBox = document.getElementById('resultBox')
+let correctBox = document.getElementById('correctBox');
 
- function toggleStyle () {
-    style.display = 'none';
+function showBox () {
+    correctBox.style.display = 'block';
 }
 
-function () {
-    style.display = 'box';
+
+
+
+// for (i = 1; i < 3; i++) {
+//     let fails = document.getElementById(`fail${i}`);
+// }
+
+let fail1 = document.getElementById('fail1');
+let fail2 = document.getElementById('fail2');
+
+fail1.onclick = function () {
+    correct.classList.add("correct");
+    fail1.classList.add('fail');
+    showBox2();
+    fail1.style.pointerEvents = 'none';
+    fail2.style.pointerEvents = 'none';
+    correct.style.pointerEvents = 'none';
+}
+
+fail2.onclick = function () {
+    correct.classList.add("correct");
+    fail2.classList.add('fail');
+    showBox2();
+    fail1.style.pointerEvents = 'none';
+    fail2.style.pointerEvents = 'none';
+    correct.style.pointerEvents = 'none';
+}
+
+let failBox = document.getElementById('failBox');
+
+function showBox2 () {
+    failBox.style.display = 'block';
 }
 
