8f081945213d16116c660088b93849a35adb4133
asaka0408 <sakanadaisuki0408@softbank.ne.jp>
2021-12-19 01:31:36 +0900

    昔のコード取得

--- src/index.php
+++ src/index.php
@@ -1,2 +1,20 @@
 <?php
-phpinfo();
+echo phpinfo();
+
+$dsn = 'mysql:host=db;dbname=quizy;charset=utf8';
+$user = 'asaka';
+$password = 'password';
+
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    // $stmt = $pdo->query("SELECT * FROM questions" );
+    // $posts = $stmt->fetchAll();
+
+    $stmt = $pdo->query("SELECT * FROM choices");
+    $posts = $stmt->fetchAll();
+    echo "接続成功\n";
+} catch (PDOException $e) {
+    echo "接続失敗: " . $e->getMessage() . "\n";
+    exit();
+}
