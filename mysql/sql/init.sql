86a3172db5f6df7cfa1c6fb11e32c1ce03daa670
asaka0408 <sakanadaisuki0408@softbank.ne.jp>
2022-01-09 14:34:06 +0900

    データの登録完了

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -1,32 +1,82 @@
-DROP TABLE IF EXISTS choices;
-CREATE TABLE choices (
-id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-big_question_id INT NOT NULL,
-question_id INT NOT NULL,
-choice0 VARCHAR(225) NOT NULL,
-choice1 VARCHAR(225) NOT NULL,
-choice2 VARCHAR(225) NOT NULL,
-image VARCHAR(225) NOT NULL
+-- DROP TABLE IF EXISTS choices; もしあったら消してね
+-- CREATE TABLE choices ( テーブル作りますよ
+-- id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, １番から自動的にIDをふってくれる。自分で振ってもいけど、勝手にやってくれたほうがありがたいよね.
+-- Primary keyとは：idは必ず固有の値ユニークであるといういみ。紐付けのときに判定できなくなるのを防ぐ。おまじない
+-- big_question_id INT NOT NULL, 必ず何かしら入力してください
+-- question_id INT NOT NULL,
+-- choice0 VARCHAR(225) NOT NULL, 文字列を格納する型（２５５字数制限）
+-- choice1 VARCHAR(225) NOT NULL,
+-- choice2 VARCHAR(225) NOT NULL,
+-- image VARCHAR(225) NOT NULL
+-- );
+-- ↑正規化されていない
+
+-- 紐付けルール：同じ型でないといけない
+
+DROP TABLE IF EXISTS prefectures;
+CREATE TABLE prefectures (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(225) NOT NULL
 );
 
-INSERT INTO choices (big_question_id, question_id, choice0, choice1, choice2, image) VALUES 
-(1, 1, 'たかなわ', 'こうわ', 'たかわ', 'Tokyo1.png'),
-(1, 2, 'かめいど', 'かめと', 'かめど', 'Tokyo2.png'),
-(1, 3, 'こうじまち', 'かゆまち', 'おかとまち', 'Tokyo3.png' ),
-(1, 4, 'おなりもん', 'おかどもん', 'ごせいもん', 'Tokyo4.png'),
-(1, 5, 'とどろき', 'たたら', 'たたろき', 'Tokyo5.png'),
-(1, 6, 'しゃくじい', 'せきこうい', 'いじい', 'Tokyo6.png'),
-(1, 7, 'ぞうしき', 'ざっしき', 'ざっしょく', 'Tokyo7.png'), 
-(1, 8, 'おかちまち', 'みとちょう', 'ごしろちょう','Tokyo8.png'),
-(1, 9, 'ししぼね', 'ろっこつ', 'しこね','Tokyo9.png'),
-(1, 10, 'こぐれ', 'こばく', 'こしゃく','Tokyo10.png'),
-(2, 1, 'むかいなだ', 'むこうひら', 'むきひら','Hiroshima1.png'),
-(2, 2, 'みつぎ', 'みよし', 'おしらべ','Hiroshima2.png'),
-(2, 3, 'かなやま', 'ぎんざん', 'きやま','Hiroshima3.png'),
-(2, 4, 'とよひ', 'とよか', 'としか','Hiroshima4.png'),
-(2, 5, 'いしぐろ', 'いしあぜ', 'しゃくぜ','Hiroshima5.png'),
-(2, 6, 'みよし', 'みかた', 'みつぎ','Hiroshima6.png'),
-(2, 7, 'うずい', 'もみち', 'くもおり','Hiroshima7.png'),
-(2, 8, 'すもも', 'ぽんかん', 'でこぽん','Hiroshima8.png'),
-(2, 9, 'おおちごとうげ', 'おうちごとうげ', 'おおちごえとうげ','Hiroshima9.png'),
-(2, 10, 'よおろほよばら', 'ていぼよはら', 'てっぽよばら','Hiroshima10.png');
+DROP TABLE IF EXISTS questions;
+CREATE TABLE questions (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  prefecture_id INT NOT NULL, 
+  `order` INT NOT NULL,
+  name VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS selections;
+CREATE TABLE selections (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  question_id INT NOT NULL,
+  name VARCHAR(225) NOT NULL,
+  is_correct boolean
+);
+
+INSERT INTO prefectures (id, name) VALUES (1, '東京'),(2, '広島');
+
+INSERT INTO questions (id, prefecture_id, `order`, name) VALUES 
+(1, 1, 1, '高輪'),
+(2, 1, 2, '亀戸');
+(1, 2, 1, '向洋');
+-- order必要？
+
+INSERT INTO selections (id, question_id, name, is_correct) VALUES 
+(1, 1, 'たかなわ', true),
+(2, 1, 'こうわ', false),
+(3, 1, 'たかわ', false);
+(4, 2, 'かめいど', true);
+(5, 2, 'かめと', false);
+(6, 2, 'かめど', false);
+(7, 1, 'あああ', true);
+(8, 1, 'わあ', false);
+(9, 1, 'やあ', false);
+
+
+-- desc prefectures; カラムを確認できるsqlコマンド
+
+
+
+-- INSERT INTO choices (big_question_id, question_id, choice0, choice1, choice2, image) VALUES 
+-- (1, 1, 'たかなわ', 'こうわ', 'たかわ', 'Tokyo1.png'),
+-- (1, 2, 'かめいど', 'かめと', 'かめど', 'Tokyo2.png'),
+-- (1, 3, 'こうじまち', 'かゆまち', 'おかとまち', 'Tokyo3.png' ),
+-- (1, 4, 'おなりもん', 'おかどもん', 'ごせいもん', 'Tokyo4.png'),
+-- (1, 5, 'とどろき', 'たたら', 'たたろき', 'Tokyo5.png'),
+-- (1, 6, 'しゃくじい', 'せきこうい', 'いじい', 'Tokyo6.png'),
+-- (1, 7, 'ぞうしき', 'ざっしき', 'ざっしょく', 'Tokyo7.png'), 
+-- (1, 8, 'おかちまち', 'みとちょう', 'ごしろちょう','Tokyo8.png'),
+-- (1, 9, 'ししぼね', 'ろっこつ', 'しこね','Tokyo9.png'),
+-- (1, 10, 'こぐれ', 'こばく', 'こしゃく','Tokyo10.png'),
+-- (2, 1, 'むかいなだ', 'むこうひら', 'むきひら','Hiroshima1.png'),
+-- (2, 2, 'みつぎ', 'みよし', 'おしらべ','Hiroshima2.png'),
+-- (2, 3, 'かなやま', 'ぎんざん', 'きやま','Hiroshima3.png'),
+-- (2, 4, 'とよひ', 'とよか', 'としか','Hiroshima4.png'),
+-- (2, 5, 'いしぐろ', 'いしあぜ', 'しゃくぜ','Hiroshima5.png'),
+-- (2, 6, 'みよし', 'みかた', 'みつぎ','Hiroshima6.png'),
+-- (2, 7, 'うずい', 'もみち', 'くもおり','Hiroshima7.png'),
+-- (2, 8, 'すもも', 'ぽんかん', 'でこぽん','Hiroshima8.png'),
+-- (2, 9, 'おおちごとうげ', 'おうちごとうげ', 'おおちごえとうげ','Hiroshima9.png'),
+-- (2, 10, 'よおろほよばら', 'ていぼよはら', 'てっぽよばら','Hiroshima10.png');
--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-echo phpinfo();
+// echo phpinfo();
 
 $dsn = 'mysql:host=db;dbname=quizy;charset=utf8';
 $user = 'asaka';
@@ -8,13 +8,33 @@ $password = 'password';
 
 try {
     $pdo = new PDO($dsn, $user, $password);
-    // $stmt = $pdo->query("SELECT * FROM questions" );
-    // $posts = $stmt->fetchAll();
-
-    $stmt = $pdo->query("SELECT * FROM choices");
-    $posts = $stmt->fetchAll();
-    echo "接続成功\n";
+   
+    $id = htmlspecialchars($_GET["id"]);
+    $stmt = $pdo->prepare("SELECT * FROM prefectures WHERE id = ?"); //choices tableから取ってきてね
+    // INNER JOIN questions ON prefectures.id = questions.prefecture_id
+    $stmt->execute(array($id));
+    $posts =$stmt->fetchAll(); 
+    // echo "接続成功\n";
+    print_r($posts);
 } catch (PDOException $e) {
     echo "接続失敗: " . $e->getMessage() . "\n";
     exit();
 }
+
+?>
+
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POSSE課題クイジー提出用</title>
+    <link rel="stylesheet" href="../css/recetCSS.css">
+    <link rel="stylesheet" href="../css/kuizy.css">
+</head>
+<body>
+    <div id="container"></div>
+    
+    <script src="../js/quizy_3.js"></script>
+</body>
+</html>
