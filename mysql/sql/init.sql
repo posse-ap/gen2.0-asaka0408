82ff8360da3fc9ef49f3a1ec748b8cc33863a00e
asaka0408 <sakanadaisuki0408@softbank.ne.jp>
2022-03-15 07:48:20 +0900

    だみーでーたとうろく

--- mysql/sql/init.sql
+++ mysql/sql/init.sql
@@ -4,13 +4,23 @@ CREATE TABLE users (
   name VARCHAR(255) NOT NULL
 );
 
+INSERT INTO users SET name='eddy';
+
 
 DROP TABLE IF EXISTS dates;
 CREATE TABLE dates (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  date INT NOT NULL,
   `user_id` INT NOT NULL
 );
+INSERT INTO dates
+  (date, `user_id`)
+VALUES
+  (2022/03/08, 1),
+  (2022/03/10, 1),
+  (2022/03/11, 1),
+  (2022/03/12, 1);
+  
 
 
 DROP TABLE IF EXISTS contents;
@@ -20,6 +30,18 @@ CREATE TABLE contents (
   date_id INT NOT NULL
 );
 
+INSERT INTO contents
+  (name, date_id)
+VALUES
+  ('N予備校', 1),
+  ('ドットインストール', 1),
+  ('N予備校', 2),
+  ('ドットインストール', 2),
+  ('N予備校', 3),
+  ('ドットインストール', 3),
+  ('N予備校', 4),
+  ('ドットインストール', 4),
+
 
 DROP TABLE IF EXISTS languages;
 CREATE TABLE languages (
@@ -28,3 +50,18 @@ CREATE TABLE languages (
   time INT NOT NULL,
   content_id INT NOT NULL
 );
+
+INSERT INTO languages
+  (name, time, content_id)
+VALUES
+  ('html', 3, 1),
+  ('css', 2, 1),
+  ('html', 3, 2),
+  ('js', 1, 2),
+  ('html', 3, 3),
+  ('html', 3, 4),
+  ('css', 1, 4),
+  ('html', 3, 5),
+  ('html', 4, 6),
+  ('html', 2, 7),
+  ('html', 1, 8),
